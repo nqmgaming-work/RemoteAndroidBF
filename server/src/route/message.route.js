8a35dd47a31b03d21097c8dd418ff66c1ef7b583
Nguyen Quang Minh <nguyenquangminh623@gmail.com>
2024-07-23 14:06:02 +0700

    feat: Add route to send messages
    This commit adds a new route `/send` to the message router to handle incoming message data and store it in the database
    .

--- server/src/route/message.route.js
+++ server/src/route/message.route.js
@@ -3,3 +3,13 @@ const router = express.Router();
 const Message = require('../model/message.model');
 
 // Create a new message
+router.post('/send', async (req, res) => {
+    try {
+        const newMessage = new Message(req.body);
+        await newMessage.save();
+        res.send({message: 'Message sent successfully', messageId: newMessage._id});
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }
+});
